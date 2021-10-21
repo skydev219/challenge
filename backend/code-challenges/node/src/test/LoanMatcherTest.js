6ed63d1e92dc556fc7cb1d7e7a9a8aa51038feaf
skydev219 <skydev.goldday219@gmail.com>
2021-10-21 17:41:50 -0300

    Ajustada msg do teste

--- backend/code-challenges/node/src/test/LoanMatcherTest.js
+++ backend/code-challenges/node/src/test/LoanMatcherTest.js
@@ -3,7 +3,7 @@ const loanMatcher = require('../main/loanMatcher');
 const assert = require('assert');
 
 describe('Challenge Test', () => {
-  it('should return 1 item with loan type PERSONAL_LOAN', () => {
+  it('should return 1 loan with type PERSONAL_LOAN', () => {
     const customer = new Customer(3000);
     const availableLoans = loanMatcher.loans(customer);
     assert.equal(availableLoans.length, 1);
